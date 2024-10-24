99b5e1c263000bebcd6b9f859ed7eae0e3e1aff2
pranali01-23 <paralepranali@gmail.com>
2024-10-24 19:30:44 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+////with api only- 3 json
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ////before API add
 // import React from 'react';
 // import ReactDOM from 'react-dom';
