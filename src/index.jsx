b9408a247a2f964943cfc36da8b53a63e5f6d2c9
pranali01-23 <paralepranali@gmail.com>
2024-10-24 19:20:30 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+////before API add
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './styles/App.css';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
